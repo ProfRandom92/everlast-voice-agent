1876261d4eae061cd2cfe336313b5e90ccbf2c16
ProfRandom92 <159939812+ProfRandom92@users.noreply.github.com>
2026-02-26 21:24:51 +0100

    Remove unused ToolExecutor import (removed in LangGraph 0.1+)

--- everlast_voice_agents/voice_agents.py
+++ everlast_voice_agents/voice_agents.py
@@ -3,7 +3,6 @@
 
 from typing import TypedDict, Annotated, Sequence, Optional, Literal
 from langgraph.graph import StateGraph, END
-from langgraph.prebuilt import ToolExecutor
 from langchain_core.messages import BaseMessage, HumanMessage, AIMessage, SystemMessage
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_anthropic import ChatAnthropic
